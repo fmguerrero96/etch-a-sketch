50bbac3e1b63da1b1222a205e7c9444d3814550c
Misael Guerrero <mguerrerowest@gmail.com>
2022-11-21 11:52:51 -0500

    Add function to change color of divs with buttons

--- main.js
+++ main.js
@@ -1,8 +1,34 @@
+let color = 'black'
+
 function createBoard(size) {
     const board = document.querySelector('#board');
 
     board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
     board.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+
+    let numDIvs = size * size;
+    for (let i = 0; i < numDIvs; i++){
+        let div = document.createElement('div');//creating cells
+        div.addEventListener('mouseover', colorDiv)
+        board.insertAdjacentElement("beforeend", div)//inserting cells into container
+
+    }
 }
 
-createBoard(50)
+function colorDiv(){
+    if (color == 'random')
+        {this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`;}
+        else if (color == 'blue')
+        {this.style.backgroundColor = 'rgb(0, 145, 255)';}
+        else if (color == 'red')
+        {this.style.backgroundColor = 'rgb(238, 6, 6)';}
+        else if (color == 'white')
+        {this.style.backgroundColor = 'rgb(240, 248, 255)';}
+        else {this.style.backgroundColor = 'black'}      
+}
+
+function changeColor(colorChoice){
+    color = colorChoice;
+
+}
+
